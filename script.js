ce4fc8312e72dbbf43eec5174d551e35ee77c172
Kaustubh Kulkarni <89833855+kaustubhdkulkarni@users.noreply.github.com>
2023-12-07 13:47:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,122 @@
+const sentencesArrayMarathi = [
+  "यह एक मराठी पाठ आहे.",
+  "आपल्या जीवनात शिक्षण, काम, आणि सामाजिक संबंधांचा एक महत्त्वाचा संगम आहे. शिक्षण आपल्या जीवनात ज्ञान आणि समज घालण्याची प्रेरणा देतो. त्यामुळे आपण आत्मविश्वासी आणि सक्षम बनतो. काम हे आपल्या स्वतंत्रतेचा प्रतीक आहे. आपले काम करण्याचे तरीके आपल्याला ओळखतात आणि आपल्याला विशेष करतात. सामाजिक संबंध आपल्या जीवनात आपल्या नैतिक मूल्यांची वाट पाडतात. इतरांसह संवाद साधण्याची अनुभव देतात आणि विचार विस्तार करतात. एकाच समयात, शिक्षणातून ज्ञान मिळतो, कामातून अनुभव मिळतो, आणि सामाजिक संबंधांतून अभिज्ञान मिळतो. त्यामुळे या तीन घटकांचे संगम हे आपल्या जीवनात एक बहुमूल्य अनुभव देते. आपल्या व्यक्तिमत्त्वात त्या तीन घटकांचे संगम एक सामर्थ्यपूर्ण आणि संपूर्ण विकास घडवतात.",
+  "क्षणिक दृष्टिकोनात, आपल्या जीवनात या तीन घटकांचा संगम आहे ज्यामुळे आपल्या व्यक्तिमत्त्वात एक अनोळखी आणि निखरीत चमक आली जाते. शिक्षणातून आपण नवीन विचारांच्या आणि दृष्टिकोनांच्या संगती सापडतो, कामातून अनुभवांच्या विस्तारातून आपल्या कौशल्यांची वृद्धी होते, आणि सामाजिक संबंधांमध्ये सामूहिक जवळच्या दृष्टिकोनाची समज होती.",
+];
+
+const sentencesArrayEnglish = [
+  "Nature, with its boundless expanse, whispers secrets of tranquility and resilience. It's an orchestration of beauty—mountains reaching for the sky, rivers carving through landscapes, and forests breathing life into the world. Within this symphony, seasons dance in harmony, painting vibrant portraits of change. Spring heralds new beginnings, with blossoms and fragrances renewing the earth, while summer's warmth envelops everything in a golden embrace. Autumn arrives, adorning trees with a kaleidoscope of colors, a majestic prelude to winter's serene hush, where snowflakes blanket the earth in a soft, peaceful slumber. Each cycle, a testament to nature's cycle of renewal and transformation. Exploring this tapestry of existence, humans forge connections with the natural world. It's in the silent conversations with the rustling leaves, the solace found in secluded nooks, and the awe-inspired gazes at the star-studded night sky. Nature is not just an observer; it’s a nurturing mentor, teaching resilience, adaptability, and harmony. Its grandeur inspires creativity, fuels contemplation, and invites us to be stewards of this planet—to protect, cherish, and nurture its fragile yet robust essence, fostering a world where the echo of nature's symphony harmonizes with the human spirit.",
+  // Second sentence
+  "As dawn breaks across the horizon, painting the sky with hues of orange and pink, a new day unfolds with promises of possibilities. The morning breeze whispers tales of untold adventures, inviting one to embrace the day's opportunities. With each sunrise, a canvas of opportunities presents itself, urging individuals to write their stories amidst the canvas of life. The day progresses, weaving a tapestry of moments—some fleeting, others etched in memory. Amidst the cacophony of bustling streets and serene retreats, lives intersect, stories entwine, and aspirations find wings to soar. In this symphony of existence, moments of joy and sorrow intertwine, each offering a glimpse into the human experience. The setting sun paints the sky with hues of warmth, bidding adieu to another day filled with lessons and revelations, yet leaving a promise of a new beginning with the break of dawn.",
+  // Third sentence
+  "Each heartbeat resonates with the rhythm of life, an intricate melody played by the symphony of existence. Emotions, the colors of our internal palette, paint the canvas of our journey. Love, a kaleidoscope of emotions, weaves through our lives, embracing us in its warmth and guiding our paths. Amidst moments of solitude, reflections unveil truths that resonate within, shaping our perceptions and nurturing our growth. Dreams, the ethereal architects of our aspirations, linger in the corridors of our minds, urging us to chase the horizon and sculpt our destinies amidst the tapestry of time.",
+  // Fourth sentence
+  "In the tapestry of cultures, diversity weaves a rich fabric, adorned with traditions, languages, and customs, each thread contributing to the vibrant mosaic of humanity. Across continents, stories echo through generations, carrying legacies and wisdom, bridging the past with the present. Celebrations resonate with laughter and shared joy, transcending boundaries and unifying hearts. In the embrace of diversity, the human spirit thrives, celebrating uniqueness and fostering understanding, painting the world with shades of inclusivity and acceptance.",
+];
+
+const sentencesArrayHindi = [
+  "हिमालय की चोटियाँ, बादलों के बीच छिपी हुई, सफेद चादर की तरह चमकती हैं। उनकी ऊंचाई और भव्यता देखने वाले को मंत्रमुग्ध कर देती है। इन पहाड़ों की छाँव में बसे गाँवों में जीवन शांत और सरल है। लोग यहाँ की प्राकृतिक सुंदरता के बीच रहकर एक सुखी और संतुष्ट जीवन जीते हैं।",
+
+  "ताजमहल, दुनिया के सात अजूबों में से एक, एक सफेद संगमरमर की इमारत है, जो अपनी खूबसूरती के लिए मशहूर है। यह इमारत मुगल बादशाह शाहजहाँ ने अपनी पत्नी मुमताज महल की याद में बनवाई थी। ताजमहल की दीवारें नक्काशी और कलाकृतियों से सजी हुई हैं, जो इसे और भी भव्य बनाती हैं।",
+
+  "गंगा नदी, भारत की पवित्र नदियों में से एक है। यह नदी भारतीय संस्कृति में एक महत्वपूर्ण स्थान रखती है। गंगा नदी के किनारे कई तीर्थस्थल और मंदिर बने हुए हैं। इस नदी के जल को पवित्र माना जाता है और लोग इसमें स्नान करना शुभ मानते हैं। गंगा नदी भारत के लोगों के लिए आस्था और श्रद्धा का केंद्र है।",
+
+  "भारतीय व्यंजन, अपनी विविधता और स्वाद के लिए जाना जाता है। विभिन्न क्षेत्रों में पाए जाने वाले विभिन्न प्रकार के भारतीय व्यंजन लोगों को अपने स्वादिष्ट और अनोखे स्वाद से आकर्षित करते हैं। भारतीय व्यंजनों में मसालों का भरपूर उपयोग किया जाता है, जो इसे और भी स्वादिष्ट बनाता है।",
+];
+
+const displayText = document.getElementById("displayText");
+const userInput = document.getElementById("userInput");
+const result = document.getElementById("result");
+let selectedlang = "";
+let textToType = "";
+let mistakes = 0;
+
+//   for lang_selection
+const dropdown = document.getElementById("inp_lang");
+dropdown.addEventListener("change", function () {
+  selectedlang = dropdown.value;
+  renderNewQuote(); // Call renderNewQuote whenever the dropdown value changes
+});
+const renderNewQuote = () => {
+  //let quote = ""; // Define quote variable
+  userInput.value = ""; //to clear textarea
+  if (selectedlang === "Marathi") {
+    const randomIndex = Math.floor(
+      Math.random() * sentencesArrayMarathi.length
+    );
+    textToType = sentencesArrayMarathi[randomIndex];
+    userInput.disabled = false
+  } else if (selectedlang === "Hindi") {
+    const randomIndex = Math.floor(Math.random() * sentencesArrayHindi.length);
+    textToType = sentencesArrayHindi[randomIndex];
+    userInput.disabled = false
+  } else {
+    const randomIndex = Math.floor(
+      Math.random() * sentencesArrayEnglish.length
+    );
+    textToType = sentencesArrayEnglish[randomIndex];
+    userInput.disabled = false
+  }
+
+  result.innerHTML = textToType;
+};
+
+
+// main code start here
+userInput.addEventListener("input", function () {
+  const userWords = userInput.value.split(" ");
+  //   console.log(userWords);
+  let feedback = "";
+  const expectedWords = textToType.split(" ");
+  //   console.log("Sentence:", expectedWords);
+
+  for (let i = 0; i < expectedWords.length; i++) {
+    if (userWords[i] === expectedWords[i] && i < userWords.length) {
+      feedback += `<span class="correct">${expectedWords[i]}</span> `;
+    } else if (userWords[i] != expectedWords[i] && i < userWords.length - 1) {
+      feedback += `<span class="incorrect">${expectedWords[i]}</span> `;
+      for (let j = i; j < userWords.length; j++) {
+        // console.log(userWords[i][j]);
+      }
+    } else if (i < userWords.length) {
+      feedback += `<span class="highlight">${expectedWords[i]}</span> `;
+    } else {
+      feedback += `<span class="default">${expectedWords[i]}</span> `;
+    }
+  }
+  console.log("Mistakes count:", mistakes);
+  //   for (let j = i; j < userWords.length; j++) {
+  //     if (userWords[i][j] != expectedWords[i][j]) {
+  //         console.log(userWords[i][j])
+  //         mistakes++;
+  //         document.getElementById("mistakes").innerHTML = mistakes
+  //     }
+  //     // console.log(userWords[i][j]);
+  //   }
+
+  // for considering only one space
+  userInput.addEventListener("keydown", function (event) {
+    if (event.key === " " && userInput.value.slice(-1) === " ") {
+      event.preventDefault();
+    }
+  });
+  result.innerHTML = feedback;
+});
+
+function countIncorrectWords() {
+    const incorrectWords = document.querySelectorAll(".incorrect");
+    const mistakesSpan = document.getElementById("mistakes");
+    let mistakeCount = 0;
+    userInput.disabled = true
+    incorrectWords.forEach(word => {
+      // Split the word to count individual words if needed
+      const words = word.textContent.split(" ");
+      mistakeCount += words.length;
+    });
+  
+    mistakesSpan.textContent = mistakeCount;
+  }
+  
+
+
+renderNewQuote();
